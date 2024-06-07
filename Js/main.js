dee6a7484a655cc78aaac85347403d9a112c71e2
Tomás Niederhauser <78279996+niederhausertomas@users.noreply.github.com>
2024-06-07 21:51:09 +0200

    wip

--- Js/main.js
+++ Js/main.js
@@ -32,15 +32,52 @@ function toggleTitulo(className) {
 
 // -------- Carrousel -------------- 
 
-function changeBannerImage(newImageSrc, title, paragraph) {
-    let bannerImage = document.querySelector('.banner img')
-    let bannerTitle = document.querySelector('.banner h5')
-    let bannerParagraph = document.querySelector('.banner p')
-    bannerImage.src = newImageSrc
-    bannerTitle.textContent = title
-    bannerParagraph.textContent = paragraph
-  }
+let bannerImage = document.querySelector('.banner img');
+let bannerTitle = document.querySelector('.banner h5');
+let bannerParagraph = document.querySelector('.banner p');
 
+function changeBannerImage(newImageSrc, title, paragraph) {
+    bannerImage.src = newImageSrc;
+    bannerTitle.textContent = title;
+    bannerParagraph.textContent = paragraph;
+}
+
+const images = [
+    { src: './images/LEVELUK K8.png', title: 'LEVELUK K8', paragraph: 'Es el equipo de alta gama de Enagic. El Leveluk K8 fue diseñado para ofrecer la máxima potencia, dado que consigue incrementar los niveles de anti-oxidación, gracias a sus 8 placas de titanio bañadas en platino, hasta en -1.000 ORP. Este modelo es multivoltaje, se prende y apaga automáticamente, ahorrando energía en hasta un 30%, y posee una pantalla táctil muy amigable que habla en 8 idiomas diferentes. Cuenta también con un sistema de limpieza semiautomático y un contador de litros para el recambio del filtro interno.' },
+    { src: './images/Leveluk-SD501-Platinum.png', title: 'LEVELUK SD-501 Platinum', paragraph: 'Este equipo de Enagic es el más vendido en el mundo, dado su excelente equilibrio precio/prestaciones. Cuenta con niveles de anti-oxidación de hasta -800 ORP, gracias a sus 7 placas de titanio bañadas en platino. El Leveluk SD501 Platinum posee un diseño plateado que se acopla muy bien a casi toda cocina y una pantalla muy amigable que habla en 5 idiomas diferentes.' },
+    { src: './images/Leveluk-SD501.png', title: 'LEVELUK SD-501', paragraph: 'En su interior, este equipo es exactamente el mismo que el Leveluk SD501 Platinum. Básicamente sus diferencias se encuentran en su diseño externo y en la cantidad de idiomas: el Leveluk SD501 habla en inglés. Al igual que los otros Leveluk' },
+    { src: './images/anespa.png', title: 'ANESPA DX', paragraph: 'Esta ducha protege tu piel y pelo brindando un efecto relajante. ANESPA-DX contiene una mezcla de ingredientes provenientes de las aguas termales naturales y minerales. Proporciona agua con iones minerales que potencian los efectos relajantes. Beneficios: produce agua especialmente eficaz contra los sarpullidos, la piel seca, problemas de circulación, eczemas, contusiones y esguinces, rigidez en los hombros, entre muchos otros beneficios. Retira completamente los residuos del cloro y bacterias del agua a través del sistema de purificación del cartucho interno.' }
+];
+
+let currentIndex = 0;
+let intervalId;
+
+function autoChangeBannerImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    changeBannerImage(images[currentIndex].src, images[currentIndex].title, images[currentIndex].paragraph);
+}
+
+function startAutoChange() {
+    intervalId = setInterval(autoChangeBannerImage, 3000);
+}
+
+function stopAutoChange() {
+    clearInterval(intervalId);
+}
+
+document.querySelectorAll('.thumbnail').forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        currentIndex = index;
+        changeBannerImage(images[currentIndex].src, images[currentIndex].title, images[currentIndex].paragraph);
+        
+        stopAutoChange();
+        setTimeout(() => {
+            startAutoChange();
+        }, 6000);
+    });
+});
+
+startAutoChange();
 
 
 
